bc728d1eee1372626251465b55fc5f980ad2c5cc
Adam Majcherek <dem0n1358v2@gmail.com>
2019-08-30 20:47:49 +0200

    Game directory initialized.

---
+++ Game/Mods/Overmodded.Commands/src/CommandsModEntry.cs
@@ -0,0 +1,19 @@
+﻿//
+// Overmodded (SANDBOX) Source
+//
+// Copyright (c) 2019 ADAM MAJCHEREK ALL RIGHTS RESERVED
+//
+
+using Overmodded.Mods.API;
+
+namespace Overmodded.Commands
+{
+    public class CommandsModEntry : ModEntry
+    {
+        /// <inheritdoc />
+        protected override void OnLoad()
+        {
+            RegisterBehaviour(new CommandsPlayer());
+        }
+    }
+}
---
+++ Game/Mods/Overmodded.Commands/src/CommandsPlayer.cs
@@ -0,0 +1,39 @@
+﻿//
+// Overmodded (SANDBOX) Source
+//
+// Copyright (c) 2019 ADAM MAJCHEREK ALL RIGHTS RESERVED
+//
+
+using Overmodded.Gameplay.Statistics;
+using Overmodded.Mods.API;
+using Overmodded.Mods.API.Commands;
+using Overmodded.Mods.API.Core.Gameplay;
+
+namespace Overmodded.Commands
+{
+    public class CommandsPlayer : ModBehaviour
+    {
+        [Command("kill", ExecMode = CmdExecMode.OnlyAsServer, Description = "Kill your self...")]
+        public void OnKill(ModCommandSender sender)
+        {
+            if (!sender.IsPlayer)
+                return;
+
+            sender.PlayerEntity.ServerKillEntity(new MDamageResult(
+                new MDamageRequest(new MDamageData(sender.PlayerEntity), sender.PlayerEntity),
+                new MDamageInfo(
+                    (int) sender.PlayerEntity.StatisticManager.GetStatistic(StatisticName.Health).FirstValue), true));
+        }
+
+        [Command("respawn", ExecMode = CmdExecMode.OnlyAsServer, Description = "Respawn your self...")]
+        public void OnRespawn(ModCommandSender sender)
+        {
+            if (!sender.IsPlayer)
+                return;
+
+            // instead of ServerRespawnEntity we should use RestartCharacter with resurrect set to true
+            sender.PlayerEntity.RestartCharacter(true);
+            //sender.PlayerEntity.ServerRespawnEntity();
+        }
+    }
+}
